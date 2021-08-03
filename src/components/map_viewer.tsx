9e8ea440014c238dafc7262f4320d425c363b0f1
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2021-08-03 22:24:54 +0200

    Draggable map

--- src/components/map_viewer.tsx
+++ src/components/map_viewer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag } from "react-use-gesture";
+import { useState } from "react";
 
 type MapProps = {
   name: string;
@@ -17,26 +20,40 @@ const BlueprintContainer = styled.div`
   width: 100%;
   display: flex;
   flex-flow: row wrap;
+  touch-action: none;
 `;
 
 const Blueprint = styled.div`
   overflow: hidden;
   flex: 0 0 50%;
   height: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const BlueprintImage = styled.img`
 `;
 
 const MapViewer = ({ blueprints }: MapProps) => {
+  const [offset, setOffset] = useState(() => ({ x: 0, y: 0 }));
+  const [blueprintStyles, blueprintAnimation] = useSpring(() => offset);
+
+  const bindDrag = useDrag(({ last, movement: [dx, dy] }) => {
+    blueprintAnimation.start({ x: offset.x + dx, y: offset.y + dy });
+
+    if (last) {
+      setOffset({
+        x: blueprintStyles.x.animation.to,
+        y: blueprintStyles.y.animation.to,
+      });
+    }
+  });
+
   return (
-    <BlueprintContainer>
+    <BlueprintContainer {...bindDrag()}>
       {blueprints.map((blueprint) => (
         <Blueprint key={blueprint.level}>
-          <BlueprintImage  src={blueprint.url} />
+          <animated.div style={blueprintStyles}>
+            <img
+              src={blueprint.url}
+              draggable={false}
+            />
+          </animated.div>
         </Blueprint>
       ))}
     </BlueprintContainer>
